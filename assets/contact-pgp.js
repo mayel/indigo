3bb49ad95273f7ad0b6ac99d947781afdb68ca5f
Mayel de Borniol <git@mayel.space>
2018-08-26 17:18:46 +0300

    switch to PGP contact form

---
+++ assets/contact-pgp.js
@@ -0,0 +1,134 @@
+const pubkey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBFuCl5IBEADVc9pybbJGxX40TPHFlWHastBlr/tNtZ7JVPQr5TDDFt43ojCa
+nvGfK1NtQH2R3n5taD3KnOoZKKV9qmfhWs84ZezGiOa9n5f/IQhtZnbGHgHr29SO
+vZLj4JmiwUyAruOAIlIUaVxvq6k+fq1UtEDiWzKDmKN9/ivBPRKYGy97NWpDEu4F
+nHdgLdchvOF7MKTQhtZgGtbDYCbfzEFd3zO+Y8MnWTWtxJrAIoQOufXW7T+9VO+P
+OhhQ4xgMkLl/a4qjdjzCdYu/uQAiH08teahS43htuHCCy3e2mf69apGPextkcU2V
+zYW80KS13q0P7c54WU1LfNAkYA7gEl6Z28A3R1f2jiet7X3tj0pixvW+HqqJwhYh
+C3DAkBvc8gj+tS/L9gQdFwAulyV7ANEmgAKEznGHCJ69BN2USl0ILShnvBmkSkkr
+ZdQByNrjC5OWmtH0JOSofQMnCoTiadyyMaCdBGU5szv/gg/dOknFqoPkPtA/IeSv
+ePJhO+eBZNaVucHVcHV5W8nX1iPu4kZRJtVx6eyV0rxjrSBmxGJ2dl99oRS7kKEi
+u9UqcMyUVd083J7jYfVWgNjCA5DtmP4wbBw8haR0g9xWPPuwxO+2l3OHXka/Gw0X
+kCxgO2V4ipaJmALqf07VxZajxWWELRnugqfrmsGEi6kd5GIZ74y6hIbRmQARAQAB
+tBNNYXllbCA8bWF5ZWxAcG0ubWU+iQJRBBMBCgA7AhsDBQsJCAcDBRUKCQgLBRYC
+AwEAAh4BAheAFiEEXlNEb3msxj04vcjDIW4vpbJflI8FAluCmAICGQEACgkQIW4v
+pbJflI8rlRAAgyCShXO8Xf8GDddHKNSryOPBbFj4SKQ+ANpTfJEUYyxms2tpMOQM
+eoL6KCBaYy+nrLc3s7L4K9nBP/bmACO1H+Yr/N2mCNBKO3lVyVf580pmBRwxDzk0
+vZJr4l7V6z1Zwmoo9N/S40+46Uw09sRQiRFd/CyffucY1jW3noZh4nvC++RcH+w4
+Ow6wWSD5CI8OYVuErfUpibg/H3KX6MExna1MeuuBY+yIJPhvaa0UNLTONo4V2DJP
+9DOFi+cs8C/Lq7F3Uq3XSdvSRgNz8mWgMYsrraEA9qecLPiibf4NURdeWLyOnN3P
+MwIbSekKMKBS0pfaaj4E9uJwtiBk5msSTBx9R6rGoZMqJFoZJXgivEJK1aVAL5We
+dMmDM2K2iJIdecOowD9E692CJl7FRBbBa9ehlI5520Hq15WIUbEeWzFudarKdVc1
+gLUBsZj0wvoSXo92PJYaBwq7OEpCBSiMin6NdXrsblXAxmCEw0IpdWIkTxW9g7kY
+Xi7aLhk+tTsLP9qyi4dqqKnf1fe8esvsu91cG8aPW+OAbLYfp5KsHhUUsWMckKsl
+uZv+wW3611dojGIM7v2Ik4/oL0lI284mcF76oDHz+kSXzQ2oJFUFAjPDs+bUsDc5
+2cZF5OHeuI978Izg1m6siyFXLQCQtYzRm6BbXnCdHH0MtSbuHwKHaeK0G01heWVs
+IDxtYXllbEBwcm90b25tYWlsLmNoPokCTgQTAQoAOBYhBF5TRG95rMY9OL3IwyFu
+L6WyX5SPBQJbgpfLAhsDBQsJCAcDBRUKCQgLBRYCAwEAAh4BAheAAAoJECFuL6Wy
+X5SPQe0QAIhTyzAqO2KMMoTtITj1RIZzHRwGPSnWWEUHMVeOzK8hmAeOlRF8pZCW
+RLfASzAS8QojFXVSrtAkIy/Ut8Y6m/zIPzwHX4xFZx0z3yIzBwN0OoEpnomAMwAo
+SZKu+q64uvGBIZKivgX3jKtLrfMLu+y7lzBijAXJYOWG6JR02xLItNraurxXcEDL
+0tbn2eoaTLWva8lIeYcSPeLryVHUmhspNInzQmX12mlUn8TmRf4JdbSf+Byq9xRE
+t0TqM0qKazhiQVGkMVczNxk0lcq9Moh0/Ih8MBYGMUXMWhO8LqhKzPeFiNaXL11s
++kWW4/M8oicHFqBUMFEGLkglUxAs51ADzlanbA95JM7X8c5JS26PMdRz+M81R7Eh
+fplT/SRj3xU6px+2sj2tsmCp0BPvp8BhtJA8+1c3szUvpGQJfuQBFyHKZtw3O4wS
+tUnLldocXk3+sEPjwvZndkCZ8KOXlLuFdZegnkF2pD25xUm5arqjBTp4fPzSSOSC
+LO16HvC7fPhgoSr69ZurFnPhI8DlJvOjQV7dGfEcE7qlivX5mIXebLfoiFnfqkXg
++RmFAUHU66lS40eQzwRb1EL4Xmb22GeCcPIR7wZNWxkfensXUCui7SgH/wsC30PL
+6dBIBmjtpuIKptK/VFI64yRSMxHPGtX8dYDpssVnEDlBSk47rcV+tBxNYXllbCA8
+bWF5ZWxAcHJvdG9ubWFpbC5jb20+iQJOBBMBCgA4FiEEXlNEb3msxj04vcjDIW4v
+pbJflI8FAluCmBgCGwMFCwkIBwMFFQoJCAsFFgIDAQACHgECF4AACgkQIW4vpbJf
+lI9d1Q//aNoZh+fOTaV+lRIDqkX7ruwDrtbCOrjA8vQYWgomvYh+EVqPYBk4+PW3
+fpI6+jb3JzJQrQaMbGu1ili1wBonpd1Y9SEtHNZmg7SrlW4R9Sx6J5twYnIXSVBV
+mcRgmqiSLERUPtu/vGiilzVr1LAkBbViNH8yZ1kgaO57H64dbIPbGoKw26v9/UiC
+FsZmry0AeIZ9x6fOC6S2t/jhDIpHvmfhQjkeu/hx9L3bHvLcGQDgDR/FLW76eutE
+kJhuZ66ELRCVM8k+Qe2RPeo7zzNRFWzRVCMM352VAaC+abiBxzlbG6RbShy+d1Xv
+1i3LwhYJc+cWI8Uk5jlP+u5o+57tfAFHuyyGqkGd7MGGk3djfKL/L6ZXWZhZWwad
+bX2pvtz9LOzatMyF8iDeCCCAdYRB6vM/Qbm1mtpMaWnZpi+DRFK/DqytLoFqc9C2
+iI2nZIRS3VlO4xVDDkZunkpfM7qCgh/YzWhacr5a+moBM2ju6Fvwzjs9SYhg7rON
+PZgbf9hFYS5pzSKZ+JXTtsehA1F2ytDyBSH/R5pGQzjEskuYuiKrmbKW37KT49mF
+qEX8uFIqIZMei6LeHU7EqzE9/aZIT5FQ4GrtQJxYQ7LcdSVy0vDjmNsUaTAbYDwd
+pj+wHpGX5SuoMxmBPbm9AMikdyzyZR1nJIDwTesMyT7s0OYw/c65Ag0EW4KXkgEQ
+AK6bsDYsJzgrKJZgvt+VwHP9rSDKIdjD+GyF2Jpq83sLPFfQ14ET9G9FDzOJu+UH
+zs9e2crLEiFbpsdyi3k/1Nbk4mzh9YoJZp6g1kPc3MSPaJESTx37SrfQSqh0p3Cc
+QKbUIBtsc5vTmQMqohsjFUT1thM04qut9JNFLJNq3rfuGJOWGYZwI06KnVvoOKuO
+qIG7UO/nuvldWa+OQcMh4Iq3jMW7z5Ul0IQNJH+MNWdC5vQy0CWhxntGBvxxnae8
+ekHnD64W+Jb9ZjcEvaltEQc7BtUoGplInu3W4qjctVaOgstFpa4RQVmmZTbZJtRO
+tulQjBBgg2rsD2qztAs92ZsVRUGHHd/6IquRztDm5jl+TKWiIgbB2sovT5vI28ES
+AsJydAqvSEj93DxeHGrF039NyWPRc6ie9rvuGEiKb+zYXg51fzhL6ELF5dZGHla7
+EciCYnqonRCmAG/CRfq1kp8Rx9GnWVxLNKmeDBYfAAAdzrvpW8JtPRI59ZnzrLh7
+w9YYEqXOMRqT8Iqu6N6AJTY0ULZAjvVMz/Qyjw0ycoLgoULFSq01JYEISVgcuGhD
+58ZMBkyq9HxDKK7hIztPJkRRIdRcA6jEL2JvmlYGcGpn3sFlweX/Z3G/aCWqrjrT
+BK1NOrSBNmObpCnBToYVK4jlKw1is0UcrUJxh979WkJJABEBAAGJAjYEGAEKACAW
+IQReU0RveazGPTi9yMMhbi+lsl+UjwUCW4KXkgIbDAAKCRAhbi+lsl+Uj9BjEACq
+570Fgp7k8rEDdftUGrNuf5tBHhisGe74S9OQZcMd+pWemHt6ffIEOZLjLNF0yllV
+H+GAE6HPImttoxaFISptQRwYXCJwBCs4kRxOWEE+ZNZm7oDYHsv9hEWBoOdlK2nu
+dE0an0Iwt8mWvh70BCcF84xZrnZ+0TIyrKpXh5LRjl6h0prTj2w5SuBrwgcrdFHv
+Lu86qhW6lCZ6+xZRtJCeNaltWW8nSm6fSXilQXkETzYv7lTipKn6C2l84AygjHB4
+31egpga7x+gG+eSeV67g3XAi7196fYoB3ufL0boiCIb3VwwOblgIysGLYKHGaMSV
+N+NUG6rdWlpP9THV1U7VueEYgWmGuTfmZHZOo6GQ06IFM/YZkhxyde2DGJG1fhdi
+DNuDvPqda+0s0Lp2rWAA7ndu/UHDcLdrFO4i/Yza2lvywV6xnFUTLpYhekKxbuKo
+/5Wio6pM8AseF+m3C1htm48xGVngwmu7FTvNgVj6hBPAKomEFJLEU8O/zTVw9J0E
+GpZHfuJXZTO8vuP18UfxRr1pho2kKy76Se4KYBNVYuw/iKXgJJd5lzFbLDLafphG
+WwI1wQQ1GPvv6G4uuLCOIp+72lNv7KCGyWccPZWv44h4ZQkRqaZ+7n7IEoTo3h/9
+neWUHIL7V3dZnZRqLlhGQfhmm+tIV+IFMUYLeNkPzQ==
+=pouw
+-----END PGP PUBLIC KEY BLOCK-----`; // Public key
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	if (window.crypto && window.crypto.getRandomValues) {
+
+		openpgp.initWorker({ path:'assets/openpgp.worker.min.js' }) // set the relative web worker path
+
+		// form ready
+		var message = document.getElementById("message");
+//     	console.log(document.forms);
+
+		// encrypt on submit
+		document.forms[0].addEventListener("submit", function(evt) {
+			evt.preventDefault();
+			if (form.elements['message'].value == '') {
+				alert('Please enter a message first!');
+			} else {
+				encrypt(message.value).then(function(encrypted_msg) {
+					form.elements['message'].value = encrypted_msg;
+					form.submit();
+				});
+				return true;
+			}
+		});
+	} else {
+		// not ready
+		document.getElementById("button").disabled = true;
+		window.alert("Error: Browser not supported\nReason: We need a cryptographically secure PRNG to be implemented (i.e. the window.crypto method)\nSolution: Use Chrome >= 11, Safari >= 3.1, Firefox >= 21, Opera >= 15 or IE >= 11.");
+		return false;
+	}
+});
+
+function encrypt(msg){
+	if (msg.indexOf("-----BEGIN PGP MESSAGE-----") !== -1 && msg.indexOf("-----END PGP MESSAGE-----") !== -1) {
+		return msg;
+
+	} else {
+		return encryptFunction(msg);
+	}
+}
+
+const encryptFunction = async(msg) => {
+
+	console.log(pubkey);
+
+	const options = {
+		message: openpgp.message.fromText(msg), // input as Message object
+		publicKeys: (await openpgp.key.readArmored(pubkey)).keys // keys for encryption
+	}
+	console.log(options);
+
+	return openpgp.encrypt(options).then(function(ciphertext) {
+		console.log(ciphertext);
+		encrypted = ciphertext.data;
+		return encrypted;
+	});
+}
--- contact.md
+++ contact.md
@@ -3,6 +3,40 @@ title: Contact
 layout: page
 ---
 
-<div style="text-align: center;">
-<script type="text/javascript"> id = 208783; clr = "-000000-f7f7f7-000042"; </script><script type="text/javascript" src="http://kontactr.com/wp.js"></script>
-</div>
+
+<script src="assets/openpgp.min.js" type="text/javascript"></script>
+<script src="assets/contact-pgp.js" type="text/javascript"></script>
+
+
+<h3>Contact me</h3>
+
+<p>This is a secure contact form with end-to-end encryption.<br/>
+Your message will be encrypted in the browser before being sent, thanks to <a href="https://openpgpjs.org" target="_blank">OpenPGP.js</a>.</p>
+
+<form id="form" method="post" action="https://blog.mayel.space/_/contact-pgp/send_form.php">
+	<table width="100%">
+		<tr>
+	  		<td valign="top">
+	   			<label for="email">Your&nbsp;Email</label>
+	 		</td>
+	  		<td valign="top">
+	   			<input type="text" name="email" style="width:100%;" required="true">
+	  		</td>
+	 	</tr>
+		<tr>
+			<td valign="top">
+				<label for="message">Message</label>
+			</td>
+			<td valign="top">
+				<textarea  id="message" name="message" style="height:180px; width:100%; min-width:420px" required="true" placeholder="If you have a PGP public key, please include a link (or copy/paste it into the message) if you want a secure reply."></textarea>
+		 </td>
+		</tr>
+		<tr>
+		  	<td >
+			</td>
+			<td >
+				<input type="submit" value="Encrypt & Send">
+		  	</td>
+		</tr>
+	</table>
+</form>
